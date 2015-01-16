b579b8df0b264b6414066f9afcdeb7ccf6b346f7
snower <sujian199@gmail.com>
2015-01-16 13:49:29 +0800

    update

--- shadowsocks/protocol/redirect.py
+++ shadowsocks/protocol/redirect.py
@@ -8,7 +8,7 @@ from protocol import Protocol,ProtocolParseEndError
 
 class RedirectProtocol(Protocol):
     def get_address(self):
-        address_data = self.request.conn.getsockopt(socket.SOL_IP, 80, 16)
+        address_data = self.request.conn.socket.getsockopt(socket.SOL_IP, 80, 16)
         self.remote_addr = socket.inet_ntoa(address_data[4:8])
         self.remote_port, = struct.unpack(">H", address_data[2:4])
 
