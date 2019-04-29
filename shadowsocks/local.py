7c3eb4375d599ce9f6bd198840d6d6f188ba2bf8
snower <sujian199@gmail.com>
2019-04-29 19:44:04 +0800

    fix

--- shadowsocks/local.py
+++ shadowsocks/local.py
@@ -119,9 +119,11 @@ class UdpPassResponse(object):
         else:
             loop.add_timeout(30, self.on_timeout)
 
-    def on_data(self, s, address, data):
-        self.recv_data_len += len(data)
-        self.request.write(self.address, address, data)
+    def on_data(self, s, address, buffer):
+        data = buffer.next()
+        while data:
+            self.request.write(self.address, address, data)
+            self.recv_data_len += len(data)
         self.data_time = time.time()
 
     def write(self, data):
